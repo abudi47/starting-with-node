1c1f14fa6570f5372a20d5c808db4d8194ac5605
abudi <abudi@abudi.abudi>
2024-08-18 18:20:10 -0400

    simple calculator that add 2 numbers

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 
 //jshint esversion:6
-const express = require("express")
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.get("/", function(req,res){
@@ -12,6 +16,22 @@ app.get("/about", function(req,res){
 });
 app.get("/contact", function(req,res){
     res.send("<h1>tg..?..</h1>")
+});
+
+
+
+
+app.post("/calculator", function(req,res){
+    console.log("thanks for using our calculator")
+
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 +  num2;
+
+    res.send("<h1>Te result is </h1>" + result)
+})
+app.get("/calculator", function(req,res){
+    res.sendFile(__dirname + "/index.html")
 });
 
 
